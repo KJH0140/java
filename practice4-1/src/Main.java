cc899775e3e0c0ea4dc7cdb25083ceff7f9b0994
KJH <kjh0140g@cju.ac.kr>
2025-03-25 11:43:33 +0900

    20250325

---
+++ practice4-1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        float Radius = 10.0f;
+        final float PI = 3.14f;
+        System.out.println(Radius*Radius*PI);
+    }
+}
